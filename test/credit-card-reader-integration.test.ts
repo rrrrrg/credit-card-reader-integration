b28dad7fee1e58dfc2274eded62f168445e17b66
Ran Guo <rguo@greenactionstudio.com>
2023-03-15 00:24:23 -0400

    init cdk project

---
+++ test/credit-card-reader-integration.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CreditCardReaderIntegration from '../lib/credit-card-reader-integration-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/credit-card-reader-integration-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CreditCardReaderIntegration.CreditCardReaderIntegrationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
